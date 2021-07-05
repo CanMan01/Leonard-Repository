27d2b9de91b4f1c5e64ce34fa70ee1ea4b43437c
CanMan01 <rappyleonard@gmail.com>
2021-07-05 14:02:20 +0800

    Create Leonard-Intro.R

---
+++ Leonard-Intro.R
@@ -0,0 +1,4 @@
+print ("Hello! My name is Raphael Leonard Lee. You can call me Leonard.")
+print("My course is BS EcE. I'm now second year.")
+print("I'm interested in cars, tech, and audio!")
+print ("Nice to meet you!")
